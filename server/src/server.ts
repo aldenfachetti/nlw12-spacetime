2c456ddabb0ca4a36d62de3303412c9ad2907310
aldenfachetti <aldenmerlin@gmail.com>
2023-05-18 17:46:23 +0200

    Fastify included

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,15 @@
-console.log("HI FIVE!");
+import fastify from "fastify";
+
+const app = fastify();
+
+app.get("/hello", () => {
+  return "hello world";
+});
+
+app
+  .listen({
+    port: 3030,
+  })
+  .then(() => {
+    console.log("🚀 HTTP server running on http://localhost:3030");
+  });
