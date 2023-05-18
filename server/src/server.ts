a73ade6ecebf064e7c81cc143db46ba30f2de520
aldenfachetti <aldenmerlin@gmail.com>
2023-05-19 00:20:53 +0200

    Mobile included and React Native set up

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,15 +5,13 @@ const app = fastify()
 const prisma = new PrismaClient()
 
 app.get('/users', async () => {
-  const users = await prisma.user.findMany()
+    const users = await prisma.user.findMany()
 
-  return users
+    return users
 })
 
-app
-  .listen({
+app.listen({
     port: 3333,
-  })
-  .then(() => {
+}).then(() => {
     console.log('🚀 HTTP server running on http://localhost:3333')
-  })
+})
--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,11 +1,5 @@
 import Image from "next/image";
-import { Button } from "../components/Button";
 
 export default function Home() {
-  return (
-    <h1>
-      Hello World! <Button /> <Button />
-      <Button />
-    </h1>
-  );
+  return <h1>Hello World!</h1>;
 }
